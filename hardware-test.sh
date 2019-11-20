56e8bb84d736fdbfd21c671315ac2b2241449d30
Ian McIntyre <ianpmcintyre@gmail.com>
2019-11-19 21:47:43 -0500

    Use cargo-binutils instead of objdump / objcopy
    Use the cargo-binutils wrapper to call the LLVM
    variants of objdump and objcopy.

--- hardware-test.sh
+++ hardware-test.sh
@@ -7,9 +7,9 @@
 cargo build --release -p teensy4-examples --bin $1 && \
     mkdir -p out && \
     cp target/thumbv7em-none-eabihf/release/$1 out/$1 && \
-    arm-none-eabi-objdump -d -S -C out/$1 > out/$1.lst && \
-    arm-none-eabi-objdump -t -C out/$1 > out/$1.sym && \
-    arm-none-eabi-objcopy -O ihex -R .eeprom out/$1 out/$1.hex
+    rust-objdump -d -S -C out/$1 > out/$1.lst && \
+    rust-objdump -t -C out/$1 > out/$1.sym && \
+    rust-objcopy -O ihex -R .eeprom out/$1 out/$1.hex
 
 if [ -x "$(command -v teensy_loader_cli)" ]; then
     teensy_loader_cli --mcu=TEENSY40 -w -v out/$1.hex
