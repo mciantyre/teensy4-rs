fe40488290caac91e21c4959bffd4d1e4a003915
Ian McIntyre <ianpmcintyre@gmail.com>
2019-12-01 20:55:09 -0500

    Revert "Move reset back into Rust"
    This reverts commit 13faddd93140c94ca40a2ccfc73e3a29bcdc1343.

--- teensy4-rt/src/lib.rs
+++ teensy4-rt/src/lib.rs
@@ -21,32 +21,12 @@ pub use nvic::exception;
 ///
 /// The function is unsafe since it directly modifies registers, and invokes
 /// other functions that do the same.
-#[link_section = ".boot.reset"]
+#[link_section = ".boot.start"]
 #[no_mangle]
-pub unsafe extern "C" fn _reset() -> ! {
+pub unsafe extern "C" fn _start() -> ! {
     extern "C" {
-        static mut __stext: u32;
-        static mut __etext: u32;
-        static __sitext: u32;
-
-        static mut __sbss: u32;
-        static mut __ebss: u32;
-
-        static mut __sdata: u32;
-        static mut __edata: u32;
-        static __sidata: u32;
-
-        static __estack: u32;
-
         fn cache_init();
-        fn tcm_init();
-        fn init_data(start: *mut u32, end: *mut u32, input: *const u32);
-        fn zero_bss(start: *mut u32, end: *mut u32);
     }
-    tcm_init();
-    init_data(&mut __stext, &mut __etext, &__sitext);
-    init_data(&mut __sdata, &mut __edata, &__sidata);
-    zero_bss(&mut __sbss, &mut __ebss);
 
     nvic::init();
     fpu::init();
