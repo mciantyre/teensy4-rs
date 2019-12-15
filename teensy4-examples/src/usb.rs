ff6fb2e9d98cc8041549a3e3b724ffbb68c86a2f
Ian McIntyre <ianpmcintyre@gmail.com>
2019-12-15 08:26:08 -0500

    Verify ARM clock timing with logs in USB example

--- teensy4-examples/src/usb.rs
+++ teensy4-examples/src/usb.rs
@@ -12,17 +12,21 @@ use embedded_hal::digital::v2::ToggleableOutputPin;
 
 #[rt::entry]
 fn main() -> ! {
-    let p = bsp::Peripherals::take().unwrap();
+    let mut p = bsp::Peripherals::take().unwrap();
     // Initialize logging with the default settings
     p.log.init(Default::default());
+    bsp::delay(2000);
+    p.ccm
+        .pll1
+        .set_arm_clock(bsp::hal::ccm::PLL1::ARM_HZ, &mut p.ccm.handle, &mut p.dcdc);
     let mut led = p.led;
     loop {
-        bsp::delay(1000);
         log::error!("Something terrible happened!");
         log::warn!("Something happened, but we fixed it");
         log::info!("It's 31'C outside");
         log::debug!("Sleeping for 1 second...");
         log::trace!("{} + {} = {}", 3, 2, 3 + 2);
         led.toggle().unwrap();
+        bsp::delay(5000);
     }
 }
