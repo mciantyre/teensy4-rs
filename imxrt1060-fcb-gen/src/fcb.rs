1bd633e5e60a2f3a50c91487457b84014d70a308
Ian McIntyre <ianpmcintyre@gmail.com>
2019-12-27 22:13:36 -0500

    Document FCB reserved offsets; prevent writes

--- imxrt1060-fcb-gen/src/fcb.rs
+++ imxrt1060-fcb-gen/src/fcb.rs
@@ -1,7 +1,21 @@
+use std::collections::HashSet;
 use std::fmt;
 use std::io;
 
 const FCB_SIZE: usize = 512;
+
+/// Offsets that are listed as 'reserved' in the main FlexSPI
+/// FCB table. We panic if we try to access one of these.
+static OFFSETS_OF_RESERVED: &[std::ops::Range<usize>] = &[
+    0x008..0x00C,
+    0x011..0x012,
+    0x01D..0x020,
+    0x02C..0x030,
+    0x03C..0x040,
+    0x048..0x050,
+    0x1B0..0x1C0,
+    0x1CC..0x200,
+];
 
 /// The firmware configuration block
 ///
@@ -10,6 +24,7 @@ const FCB_SIZE: usize = 512;
 pub struct FCB {
     pub(crate) raw: [u8; FCB_SIZE],
     comments: Vec<String>,
+    reserved: HashSet<usize>,
 }
 
 impl FCB {
@@ -17,6 +32,7 @@ impl FCB {
         let mut fcb = FCB {
             raw: [0; FCB_SIZE],
             comments: vec![String::new(); FCB_SIZE],
+            reserved: OFFSETS_OF_RESERVED.iter().cloned().flatten().collect(),
         };
 
         fcb.comments[0] = String::from("Tag 'FCFB'");
@@ -28,11 +44,19 @@ impl FCB {
         fcb.comments[7] = String::from("Version 'V'");
         fcb.raw[4..8].copy_from_slice(&0x5601_0000u32.to_le_bytes());
 
+        for reserved_offset in fcb.reserved.iter() {
+            fcb.comments[*reserved_offset] = String::from("RESERVED");
+        }
+
         fcb
     }
 
     pub(super) fn field(&mut self, offset: usize, bytes: &[u8]) {
-        self.raw[offset..offset + bytes.len()].copy_from_slice(bytes);
+        if self.reserved.contains(&offset) {
+            panic!("Attempting to access reserved offset 0x{:03X} in the FCB", offset);
+        } else {
+            self.raw[offset..offset + bytes.len()].copy_from_slice(bytes);
+        }
     }
 
     pub(super) fn field_comment<S: ToString>(&mut self, offset: usize, bytes: &[u8], comment: S) {
