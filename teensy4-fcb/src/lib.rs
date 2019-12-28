0e62b6030079a828701faf43bb79da7637986838
Ian McIntyre <ianpmcintyre@gmail.com>
2019-12-27 21:52:20 -0500

    Document teensy4-fcb crate

--- teensy4-fcb/src/lib.rs
+++ teensy4-fcb/src/lib.rs
@@ -1,3 +1,9 @@
+//! Firmware Configuration Block (FCB) for the Teensy 4
+//!
+//! See the `imxrt1060-fcb-pac` crate for details on how
+//! this was generated. See the accompanying `build.rs`
+//! for a demonstration of the API.
+
 #![no_std]
 
 include!(concat!(env!("OUT_DIR"), "/fcb.rs"));
