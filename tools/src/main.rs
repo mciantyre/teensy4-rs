4d569ed4d4a9b89c6d57f8c2959f6f98f43f308f
Ian McIntyre <ianpmcintyre@gmail.com>
2019-12-29 15:02:06 -0500

    Add some comments to the import tool; clippy cleanup

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,6 +1,9 @@
 //! Allows us to auto-import a iMXRT1062 module
 //! from the svd2rust output into the imxrt1062-pac
 //! megacrate. It code-ifies some manual work.
+//! 
+//! We expect that this tool is run from the `tools`
+//! directory.
 //!
 //! This could probably use some better error handling...
 
@@ -12,16 +15,25 @@ use std::process;
 
 mod toml;
 
-static CARGO_TOML_DEPENDENCIES: &str = r#"vcell = "0.1.2"
+/// The name of the PAC crate into which we will
+/// put subcrates
+static OUTPUT_PAC_NAME: &str = "imxrt1062-pac";
+
+/// Add dependencies into the Cargo.toml of the new PAC subcrate
+/// identified by crate_path
+fn add_deps(crate_path: &Path) {
+    /// Dependencies that are added to each PAC subcrate
+    static CARGO_TOML_DEPENDENCIES: &str = r#"vcell = "0.1.2"
 "#;
 
-static CARGO_NO_TESTS_BENCH: &str = r#"
+    /// Additional directives to add into each PAC subcrate's
+    /// Cargo.toml
+    static CARGO_NO_TESTS_BENCH: &str = r#"
 [lib]
 bench = false
 test = false
 "#;
 
-fn add_deps(crate_path: &Path) {
     let mut cargo_toml = fs::OpenOptions::new()
         .append(true)
         .open(crate_path.join("Cargo.toml"))
@@ -36,7 +48,10 @@ fn add_deps(crate_path: &Path) {
         .expect("Failed to update Cargo.toml");
 }
 
-static LIB_PRELUDE: &str = r#"#![deny(warnings)]
+/// Migrate the `lib.rs` of the PAC subscrate, adding
+/// our necessary header to the top of the file.
+fn write_lib<R: Read>(crate_path: &Path, mut src: R) {
+    static LIB_PRELUDE: &str = r#"#![deny(warnings)]
 #![allow(non_camel_case_types)]
 #![allow(clippy::all)]
 #![no_std]
@@ -44,8 +59,6 @@ static LIB_PRELUDE: &str = r#"#![deny(warnings)]
 include!("../../generic.rs");
 
 "#;
-
-fn write_lib<R: Read>(crate_path: &Path, mut src: R) {
     let mut crate_lib =
         fs::File::create(crate_path.join("src").join("lib.rs")).expect("Unable to crate lib.rs");
     crate_lib
@@ -54,6 +67,7 @@ fn write_lib<R: Read>(crate_path: &Path, mut src: R) {
     io::copy(&mut src, &mut crate_lib).unwrap();
 }
 
+/// Recursively copies the files from the provided iterator into the directory `crate_path`
 fn copy_contents<I: Iterator<Item = io::Result<fs::DirEntry>>>(crate_path: &Path, dir: I) {
     for entry in dir {
         let entry = entry.unwrap();
@@ -71,13 +85,16 @@ fn copy_contents<I: Iterator<Item = io::Result<fs::DirEntry>>>(crate_path: &Path
     }
 }
 
+/// We don't modify the PAC's `lib.rs` with the re-exports. To help
+/// our a user, suggest the new re-exports. They may copy and paste
+/// this into the file.
 fn suggest_reexports(crate_names: &[String]) {
     println!("imxrt1062-pac reexport additions...");
     for crate_name in crate_names {
         let crate_name = crate_name.replace("-", "_");
         // pub use imxrt1062_foo_bar as foo_bar
         let module = crate_name
-            .split("_")
+            .split('_')
             .skip(1)
             .map(String::from)
             .collect::<Vec<String>>()
@@ -103,6 +120,7 @@ fn update_workspace_toml(crate_names: &[String]) {
     fs::write(WORKSPACE_CARGO_TOML, new_toml).unwrap();
 }
 
+/// Add the new dependencies into the top-level PAC's Cargo.toml
 fn update_pac_dependencies(output_pac: &Path, crate_names: &[String]) {
     let output_pac_toml = output_pac.join("Cargo.toml");
     let mut krate: toml::Krate = {
@@ -115,8 +133,6 @@ fn update_pac_dependencies(output_pac: &Path, crate_names: &[String]) {
     let new_toml = ::toml::ser::to_string_pretty(&krate).unwrap();
     fs::write(&output_pac_toml, new_toml).unwrap();
 }
-
-static OUTPUT_PAC_NAME: &str = "imxrt1062-pac";
 
 fn main() {
     let output_pac: PathBuf = PathBuf::from("../").join(OUTPUT_PAC_NAME);
@@ -137,10 +153,9 @@ fn main() {
                 .join("src")
                 .join(format!("{}.rs", module_name)),
         )
-        .expect(&format!("Unable to find main module for {}", module_name));
-        let peripheral_dir_src = fs::read_dir(svd_crate_path.join("src").join(module_name)).expect(
-            &format!("Unable to find module directory for {}", module_name),
-        );
+        .unwrap_or_else(|_| panic!("Unable to find main module for {}", module_name));
+        let peripheral_dir_src = fs::read_dir(svd_crate_path.join("src").join(module_name))
+            .unwrap_or_else(|_| panic!("Unable to find module directory for {}", module_name));
 
         let crate_name = format!("imxrt1062-{}", module_name.replace("_", "-"));
         let peripheral_crate_path = output_pac.join(crate_name.clone());
@@ -160,10 +175,7 @@ fn main() {
                 "none",
             ])
             .output()
-            .expect(&format!(
-                "Cannot create peripheral crate for '{}'",
-                module_name
-            ));
+            .unwrap_or_else(|_| panic!("Cannot create peripheral crate for '{}'", module_name));
 
         add_deps(&peripheral_crate_path);
         write_lib(&peripheral_crate_path, peripheral_module_src);
