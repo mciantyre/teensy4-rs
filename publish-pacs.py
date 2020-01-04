128280353ad2a3f5630c851344848f97ec0fe39b
Ian McIntyre <ianpmcintyre@gmail.com>
2020-01-03 20:19:43 -0500

    Naively allow us to skip publishing PAC subcrates

--- publish-pacs.py
+++ publish-pacs.py
@@ -16,11 +16,23 @@ def build_cargo_publish(dry_run, allow_dirty):
             "--manifest-path", pathlib.Path(crate_path) / "Cargo.toml",
             "--target", "thumbv7em-none-eabihf"
         ]
+
         if dry_run:
             cmd.append("--dry-run")
         if allow_dirty:
             cmd.append("--allow-dirty")
-        subprocess.run(cmd, check=True)
+
+        try:
+            subprocess.run(cmd, check=True, capture_output=True)
+            dry_run_notice = "(dry run)" if dry_run else ""
+            print(f"Crate '{crate_path}' published OK {dry_run_notice}")
+        except subprocess.CalledProcessError as e:
+            if b"crate version" in e.stderr and b"is already uploaded" in e.stderr:
+                print(f"publish-pacs: skipping crate '{crate_path}', since the version already exists in crates.io")
+            else:
+                print(e)
+                raise
+        
     return _call
 
 parser = argparse.ArgumentParser(description="Automate the publishing of crates from this repo")
