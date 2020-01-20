dd90391dfd9a25239dfd0d1405ba953142feb533
Ian McIntyre <ianpmcintyre@gmail.com>
2020-01-20 16:38:57 -0500

    Use i2c example to read MPU's WHO_AM_I register

--- teensy4-examples/src/i2c.rs
+++ teensy4-examples/src/i2c.rs
@@ -1,20 +1,34 @@
-//! Demonstrates an I2C master
+//! Demonstrates an I2C master. We try to read data from
+//! a MPU9250 9-DOF IMU.
 
 #![no_std]
 #![no_main]
 
 extern crate panic_halt;
 
-use embedded_hal::blocking::i2c::Write;
+use embedded_hal::blocking::i2c;
 use teensy4_bsp as bsp;
 
-const SLAVE_ADDRESS: u8 = 0x67;
+const SLAVE_ADDRESS: u8 = 0x68; // MPU9250
+
+/// Returns the MPU's WHO_AM_I value. This should be a static
+/// value that's specific for a MPU variant.
+fn who_am_i<I>(i2c: &mut I) -> Result<u8, I::Error>
+where
+    I: i2c::WriteRead
+{
+    const WHO_AM_I: u8 = 0x75;
+    let mut out = [0; 1];
+    i2c.write_read(SLAVE_ADDRESS, &[WHO_AM_I], &mut out)?;
+    Ok(out[0])
+}
 
 #[bsp::rt::entry]
 fn main() -> ! {
     let mut peripherals = bsp::Peripherals::take().unwrap();
     peripherals.log.init(Default::default());
     bsp::delay(5000);
+
     log::info!("Enabling I2C clocks...");
     let (_, _, i2c3_builder, _) = peripherals.i2c.clock(
         &mut peripherals.ccm.handle,
@@ -29,14 +43,15 @@ fn main() -> ! {
     if let Err(err) = i2c3.set_pin_low_timeout(core::time::Duration::from_millis(1)) {
         log::warn!("Error when setting pin low timeout: {:?}", err);
     }
+
     log::info!("Starting I/O loop...");
     loop {
         bsp::delay(1000);
-        log::info!("Writing [1, 2, 3] to slave address {}", SLAVE_ADDRESS);
-        match i2c3.write(SLAVE_ADDRESS, &[1, 2, 3]) {
-            Ok(_) => log::info!("Write OK"),
+        log::info!("Querying WHO_AM_I...");
+        match who_am_i(&mut i2c3) {
+            Ok(who) => log::info!("Received 0x{:X} for WHO_AM_I", who),
             Err(err) => {
-                log::error!("Error: {:?}", err);
+                log::warn!("Error reading WHO_AM_I: {:?}", err);
                 continue;
             }
         }
