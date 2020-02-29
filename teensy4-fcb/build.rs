abd14bee3b13f7b3f082f5fd20226502d906e3e8
Ian McIntyre <ianpmcintyre@gmail.com>
2020-02-29 14:26:45 -0500

    Update usage of boot-gen
    API changed in the boot-gen crate, so we need to update our usage here.

--- teensy4-fcb/build.rs
+++ teensy4-fcb/build.rs
@@ -91,10 +91,11 @@ use std::path::Path;
 
 fn main() {
     let nor_cb = nor::ConfigurationBlock {
-        page_size: nor::PageSize::new(256),
-        sector_size: nor::SectorSize::new(4096),
+        page_size: 256,
+        sector_size: 4096,
         ip_cmd_serial_clk_freq: nor::SerialClockFrequency::MHz30,
     };
+
     let lookup_table = {
         use imxrt_boot_gen::serial_flash::CommandSequence::*;
         let mut lut = LookupTable::new();
@@ -106,24 +107,19 @@ fn main() {
         lut[ChipErase] = SEQ_CHIP_ERASE;
         lut
     };
-    let builder = Builder {
-        read_sample_clock_source: ReadSampleClockSource::LoopbackFromDQSPad,
-        cs_hold_time: CSHoldTime::new(0x01),
-        cs_setup_time: CSSetupTime::new(0x02),
-        column_address_width: ColumnAddressWidth::other_devices(),
-        device_mode_configuration: DeviceModeConfiguration::Disabled,
-        wait_time_cfg_commands: WaitTimeConfigurationCommands::disable(),
-        device_mode_seq: DeviceModeSequence::new(0, 0),
-        flash_a1_size: SerialFlashSize::new(0x0020_0000),
-        flash_a2_size: SerialFlashSize::default(),
-        flash_b1_size: SerialFlashSize::default(),
-        flash_b2_size: SerialFlashSize::default(),
-        serial_clk_freq: SerialClockFrequency::MHz60,
-        serial_flash_pad_type: FlashPadType::Quad,
-        device_type: DeviceType::SerialNOR(nor_cb),
-        lookup_table,
-    };
-    let fcb = builder.build().unwrap();
+
+    let fcb = FCBBuilder::new(DeviceType::SerialNOR(nor_cb), lookup_table)
+        .read_sample_clk_src(ReadSampleClockSource::LoopbackFromDQSPad)
+        .cs_hold_time(0x01)
+        .cs_setup_time(0x02)
+        .column_address_width(ColumnAddressWidth::OtherDevices)
+        .device_mode_configuration(DeviceModeConfiguration::Disabled)
+        .wait_time_cfg_commands(WaitTimeConfigurationCommands::disable())
+        .flash_size(SerialFlashRegion::A1, 0x0020_0000)
+        .serial_clk_freq(SerialClockFrequency::MHz60)
+        .serial_flash_pad_type(FlashPadType::Quad)
+        .build()
+        .unwrap();
 
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("fcb.rs");
