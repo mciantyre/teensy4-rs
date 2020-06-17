df86b3ae66ca4f8832e0ba6653b0e46feecfd1ab
Ian McIntyre <ianpmcintyre@gmail.com>
2020-06-17 19:39:01 -0400

    Precompile teensy4-rt C sources

--- teensy4-rt/build.rs
+++ teensy4-rt/build.rs
@@ -1,35 +1,13 @@
-static C_SRCS: &[&str] = &["./src/fault.c", "./src/reset.c"];
-
 use std::env;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Write;
 use std::path::PathBuf;
-
-/// The C compiler
-static CC: &str = "arm-none-eabi-gcc";
-/// The archiver
-static AR: &str = "arm-none-eabi-gcc-ar";
-/// Compiler flags
-static CFLAGS: &[&str] = &[
-    "-c",
-    "-Wall",
-    "-MMD",
-    "-g",
-    "-O2",
-    "-ffunction-sections",
-    "-fdata-sections",
-    "-mcpu=cortex-m7",
-    "-mthumb",
-    "-mfloat-abi=hard",
-    "-mfpu=fpv5-d16",
-    "-std=gnu11",
-];
 
 /// Files to watch for changes
 static RERUN_IF_CHANGED: &[&str] = &["build.rs", "link.x"];
 
 fn main() {
-    for &rerun_if_changed in RERUN_IF_CHANGED.iter().chain(C_SRCS.iter()) {
+    for &rerun_if_changed in RERUN_IF_CHANGED.iter() {
         println!("cargo:rerun-if-changed={}", rerun_if_changed);
     }
 
@@ -38,15 +16,8 @@ fn main() {
     let link_x = include_bytes!("link.x");
     let mut script = File::create(out_dir.join("link.x")).unwrap();
     script.write_all(link_x).unwrap();
-    println!("cargo:rustc-link-search={}", out_dir.display());
 
-    let mut builder = cc::Build::new();
-    builder.compiler(CC);
-    builder.archiver(AR);
-    builder.no_default_flags(true);
-    builder.files(C_SRCS);
-    for &flag in CFLAGS.iter() {
-        builder.flag(flag);
-    }
-    builder.compile("boot");
+    fs::copy("./bin/libboot.a", out_dir.join("libboot.a")).unwrap();
+    println!("cargo:rustc-link-lib=static=boot");
+    println!("cargo:rustc-link-search={}", out_dir.display());
 }
