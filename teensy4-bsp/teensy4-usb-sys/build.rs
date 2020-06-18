a66c21a7ebcf30db8aef79adbac91bd90f36438d
Ian McIntyre <ianpmcintyre@gmail.com>
2020-06-17 20:24:16 -0400

    Fix USB linking; rename boot to t4boot

--- teensy4-bsp/teensy4-usb-sys/build.rs
+++ teensy4-bsp/teensy4-usb-sys/build.rs
@@ -5,6 +5,5 @@ use std::path::PathBuf;
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     fs::copy("./bin/libt4usb.a", out_dir.join("libt4usb.a")).unwrap();
-    println!("cargo:rustc-link-lib=static=t4usb");
     println!("cargo:rustc-link-search={}", out_dir.display());
 }
--- teensy4-rt/build.rs
+++ teensy4-rt/build.rs
@@ -17,7 +17,7 @@ fn main() {
     let mut script = File::create(out_dir.join("link.x")).unwrap();
     script.write_all(link_x).unwrap();
 
-    fs::copy("./bin/libboot.a", out_dir.join("libboot.a")).unwrap();
-    println!("cargo:rustc-link-lib=static=boot");
+    fs::copy("./bin/libt4boot.a", out_dir.join("libt4boot.a")).unwrap();
+    println!("cargo:rustc-link-lib=static=t4boot");
     println!("cargo:rustc-link-search={}", out_dir.display());
 }
