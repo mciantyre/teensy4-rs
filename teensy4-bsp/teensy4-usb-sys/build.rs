b1ec9d8b934f8e206bdff0a4eb25cd829bff7508
Ian McIntyre <ianpmcintyre@gmail.com>
2020-06-17 20:18:41 -0400

    Precompile the USB stack

--- teensy4-bsp/teensy4-usb-sys/build.rs
+++ teensy4-bsp/teensy4-usb-sys/build.rs
@@ -1,54 +1,10 @@
-static C_SRCS: &[&str] = &[
-    "./src/nonstd.c",
-    "./src/usb_desc.c",
-    "./src/usb_serial.c",
-    "./src/usb_startup.c",
-    "./src/usb.c",
-];
-
-/// The C compiler
-static CC: &str = "arm-none-eabi-gcc";
-/// The archiver
-static AR: &str = "arm-none-eabi-gcc-ar";
-/// Compiler flags
-static CFLAGS: &[&str] = &[
-    "-c",
-    "-Wall",
-    "-MMD",
-    "-g",
-    "-O2",
-    "-ffunction-sections",
-    "-fdata-sections",
-    "-mcpu=cortex-m7",
-    "-mthumb",
-    "-mfloat-abi=hard",
-    "-mfpu=fpv5-d16",
-    "-std=gnu11",
-];
-/// Preprocessor flags
-static CPPFLAGS: &[&str] = &[
-    "-DUSB_SERIAL",
-    "-D__IMXRT1062__",
-    // TODO figure out how to handle / alias these
-    "-DFLASHMEM=__attribute__((section(\".flashmem\")))",
-    "-DPROGMEM=__attribute__((section(\".progmem\")))",
-    "-DDMAMEM=__attribute__ ((section(\".dmabuffers\"), used))",
-];
+use std::env;
+use std::fs;
+use std::path::PathBuf;
 
 fn main() {
-    for &rerun_if_changed in C_SRCS.iter() {
-        println!("cargo:rerun-if-changed={}", rerun_if_changed);
-    }
-
-    let mut builder = cc::Build::new();
-    builder.compiler(CC);
-    builder.archiver(AR);
-    builder.no_default_flags(true);
-    builder.files(C_SRCS);
-    builder.include("./src/");
-    // Add all flags
-    for &flag in CFLAGS.iter().chain(CPPFLAGS.iter()) {
-        builder.flag(flag);
-    }
-    builder.compile("usbsys");
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    fs::copy("./bin/libt4usb.a", out_dir.join("libt4usb.a")).unwrap();
+    println!("cargo:rustc-link-lib=static=t4usb");
+    println!("cargo:rustc-link-search={}", out_dir.display());
 }
