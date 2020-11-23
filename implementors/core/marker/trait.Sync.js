6e2f6252ab233958021d8ade9b67431bc3c0c7ca
mciantyre <mciantyre@users.noreply.github.com>
2020-11-23 21:52:58 +0000

    deploy: 71e860d9f0ef2ab3281bc88f4e6fe8048238339e

--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["teensy4_bsp"] = [{"text":"impl !Sync for SysTick","synthetic":true,"types":[]},{"text":"impl Sync for LoggingConfig","synthetic":true,"types":[]},{"text":"impl !Sync for Writer","synthetic":true,"types":[]},{"text":"impl !Sync for Reader","synthetic":true,"types":[]},{"text":"impl Sync for Error","synthetic":true,"types":[]}];
+implementors["teensy4_bsp"] = [{"text":"impl !Sync for SysTick","synthetic":true,"types":[]},{"text":"impl Sync for LoggingConfig","synthetic":true,"types":[]},{"text":"impl Sync for Error","synthetic":true,"types":[]},{"text":"impl !Sync for Writer","synthetic":true,"types":[]},{"text":"impl !Sync for Reader","synthetic":true,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
