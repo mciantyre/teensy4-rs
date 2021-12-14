a79246d11fd08d37558bf567d10328f7a4daa2d7
Ian McIntyre <ianpmcintyre@gmail.com>
2021-12-13 20:33:33 -0500

    Support optional logging in teensy4-panic
    The panic handler now has an optional log feature. If enabled, the
    default panic handler will log the panic message at the error level.
    
    Try the new example to demonstrate the feature with the USB logger:
    
      cargo run --example panic_log --target=thumbv7em-none-eabihf --release --all-features
    
    An ISR-driven USB logger should be able to emits its log message. But,
    this might not be true if the panic happens within a critical section,
    or when the USB ISR is masked. There's a general caveat in the crate
    documentation, stating that users are responsible for making sure they
    can see the panic message.

--- teensy4-panic/README.md
+++ teensy4-panic/README.md
@@ -1,6 +1,6 @@
 # teensy4-panic
 
-Panic handler for the Teensy 4
+Panic handler for the Teensy 4.
 
 When you link `teensy4-panic` into your program, any `panic!()` will cause
 your Teensy's LED to blink S.O.S. in Morse code. Supports both Teensy 4.0 and
@@ -21,12 +21,24 @@ Then, include the crate in your final program:
 use teensy4_panic as _;
 ```
 
-Finally, use `panic!()` to stop the program, and blink the LED.
+Finally, use `panic!()` to stop the program and blink the LED.
 
-## Custom panic handlers
+## Features
+
+The table below summarizes this crate's features. Each subsection details the feature.
+
+| Feature         |         Description                                | Default feature? |
+| --------------- | -------------------------------------------------- | ---------------- |
+| `panic-handler` | Define the Teensy 4's panic handler in this crate  |        ✓         |
+| `log`           | Log the panic message using `log::error!`          |                  |
+
+It does not make sense to _disable_ `panic-handler` and _enable_ `log`, since logging can
+only happen when `panic-handler` is enabled.
+
+### Custom panic handlers
 
 By default, `teensy4-panic` enables the `panic-handler` feature. If you want
-to use the S.O.S. routine in your own, custom panic handler, disable the default
+to use the S.O.S. routine in your own panic handler, disable the default
 features, and call `sos()`:
 
 ```toml
@@ -40,9 +52,28 @@ use teensy4_panic::sos;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
-    // ...
+    // Your panic handler here...
     sos()
 }
 ```
+
+### Log the panic message
+
+If the `log` feature is enabled, the crate links with [the `log` crate](https://crates.io/crates/log).
+Before blinking, the panic handler logs the panic message and source location at the error priority,
+using `log::error!`. The logging target is `teensy4_panic`.
+
+The example below shows a `panic!` and an example of its corresponding log message:
+
+```rust
+const DELAY_MS: u32 = 5_000;
+panic!("This is a panic message written after {}ms", DELAY_MS);
+```
+```
+[ERROR teensy4_panic]: panicked at 'This is a panic message written after 5000ms', examples/panic_log.rs:22:5
+```
+
+The panic handler only emits the log message once. You're responsible for making sure that the
+log message can reach its destination while a `panic!` is active.
 
 License: MIT OR Apache-2.0
--- teensy4-panic/src/lib.rs
+++ teensy4-panic/src/lib.rs
@@ -1,4 +1,4 @@
-//! Panic handler for the Teensy 4
+//! Panic handler for the Teensy 4.
 //!
 //! When you link `teensy4-panic` into your program, any `panic!()` will cause
 //! your Teensy's LED to blink S.O.S. in Morse code. Supports both Teensy 4.0 and
@@ -19,12 +19,24 @@
 //! use teensy4_panic as _;
 //! ```
 //!
-//! Finally, use `panic!()` to stop the program, and blink the LED.
+//! Finally, use `panic!()` to stop the program and blink the LED.
 //!
-//! # Custom panic handlers
+//! # Features
+//!
+//! The table below summarizes this crate's features. Each subsection details the feature.
+//!
+//! | Feature         |         Description                                | Default feature? |
+//! | --------------- | -------------------------------------------------- | ---------------- |
+//! | `panic-handler` | Define the Teensy 4's panic handler in this crate  |        ✓         |
+//! | `log`           | Log the panic message using `log::error!`          |                  |
+//!
+//! It does not make sense to _disable_ `panic-handler` and _enable_ `log`, since logging can
+//! only happen when `panic-handler` is enabled.
+//!
+//! ## Custom panic handlers
 //!
 //! By default, `teensy4-panic` enables the `panic-handler` feature. If you want
-//! to use the S.O.S. routine in your own, custom panic handler, disable the default
+//! to use the S.O.S. routine in your own panic handler, disable the default
 //! features, and call `sos()`:
 //!
 //! ```toml
@@ -40,12 +52,31 @@
 //!
 //! #[panic_handler]
 //! fn panic(_: &core::panic::PanicInfo) -> ! {
-//!     // ...
+//!     // Your panic handler here...
 //!     sos()
 //! }
 //! # fn main() {}
 //! # #[lang = "eh_personality"] extern fn rust_eh_personality() {}
 //! ```
+//!
+//! ## Log the panic message
+//!
+//! If the `log` feature is enabled, the crate links with [the `log` crate](https://crates.io/crates/log).
+//! Before blinking, the panic handler logs the panic message and source location at the error priority,
+//! using `log::error!`. The logging target is `teensy4_panic`.
+//!
+//! The example below shows a `panic!` and an example of its corresponding log message:
+//!
+//! ```no_run
+//! const DELAY_MS: u32 = 5_000;
+//! panic!("This is a panic message written after {}ms", DELAY_MS);
+//! ```
+//! ```text
+//! [ERROR teensy4_panic]: panicked at 'This is a panic message written after 5000ms', examples/panic_log.rs:22:5
+//! ```
+//!
+//! The panic handler only emits the log message once. You're responsible for making sure that the
+//! log message can reach its destination while a `panic!` is active.
 
 #![no_std]
 
@@ -123,7 +154,16 @@ fn o(led: &mut Led) {
 
 #[cfg(feature = "panic-handler")]
 #[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    #[cfg(feature = "log")]
+    {
+        use core::sync::atomic::{AtomicBool, Ordering};
+        // If logging results in another panic, we shouldn't try again.
+        static TRY_TO_LOG: AtomicBool = AtomicBool::new(true);
+        if TRY_TO_LOG.fetch_and(false, Ordering::Relaxed) {
+            log::error!("{}", _info);
+        }
+    }
     sos()
 }
 
