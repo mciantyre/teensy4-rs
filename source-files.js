ea4eef3408f5cf26b39481e7e342de90df104c0d
mciantyre <mciantyre@users.noreply.github.com>
2021-12-29 22:20:48 +0000

    deploy: 3f33055421a138212800f323688bed1d265f3771

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["teensy4_bsp"] = {"name":"","dirs":[{"name":"usb","files":["bindings.rs","filters.rs"]}],"files":["lib.rs","rt.rs","systick.rs","usb.rs"]};
+sourcesIndex["teensy4_bsp"] = {"name":"","dirs":[{"name":"usb","files":["bindings.rs","filters.rs"]}],"files":["lib.rs","rt.rs","usb.rs"]};
 sourcesIndex["teensy4_fcb"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["teensy4_panic"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["teensy4_pins"] = {"name":"","files":["common.rs","lib.rs","t40.rs","t41.rs"]};
