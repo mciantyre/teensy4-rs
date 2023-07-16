7416677c924e95b28eb51e99a392c044417270e1
mciantyre <mciantyre@users.noreply.github.com>
2023-07-16 12:24:21 +0000

    deploy: 3ee42a223e2f64004e53ac7586235897e56802a2

--- src-files.js
+++ src-files.js
@@ -1,7 +1,7 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "teensy4_bsp":["",[],["board.rs","clock_power.rs","lib.rs"]],\
 "teensy4_fcb":["",[],["lib.rs"]],\
 "teensy4_panic":["",[],["lib.rs"]],\
 "teensy4_pins":["",[],["common.rs","lib.rs","t40.rs","t41.rs","tmm.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
