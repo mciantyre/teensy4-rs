e06a899f9bac853f7f837818321059d3dfdc76aa
mciantyre <mciantyre@users.noreply.github.com>
2023-12-26 13:18:48 +0000

    deploy: 79402711f45f7af382cf7381b2e41c4a0324072c

--- src-files.js
+++ src-files.js
@@ -1,7 +1,7 @@
-var srcIndex = JSON.parse('{\
-"teensy4_bsp":["",[],["board.rs","clock_power.rs","lib.rs"]],\
-"teensy4_fcb":["",[],["lib.rs"]],\
-"teensy4_panic":["",[],["lib.rs"]],\
-"teensy4_pins":["",[],["common.rs","lib.rs","t40.rs","t41.rs","tmm.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["teensy4_bsp",["",[],["board.rs","clock_power.rs","lib.rs"]]],\
+["teensy4_fcb",["",[],["lib.rs"]]],\
+["teensy4_panic",["",[],["lib.rs"]]],\
+["teensy4_pins",["",[],["common.rs","lib.rs","t40.rs","t41.rs","tmm.rs"]]]\
+]'));
 createSrcSidebar();
