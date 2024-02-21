a1bba2782bbe68813ca42945065a41e77d26710b
Ben Stern <bas-github@kukaret.com>
2024-02-20 21:13:51 -0500

    use environment to override stack/heap sizes

--- build.rs
+++ build.rs
@@ -1,6 +1,15 @@
 #[cfg(feature = "rt")]
+use std::env;
+
+const DEFAULT_HEAP_SIZE: usize = 16 * 1024;
+const DEFAULT_STACK_SIZE: usize = 16 * 1024;
+
+#[cfg(feature = "rt")]
 fn main() {
     use imxrt_rt::{Family, FlexRamBanks, Memory, RuntimeBuilder};
+
+    let heap_size = env::var("TEENSY4_HEAP_SIZE").map(|val| val.parse::<usize>().unwrap_or(DEFAULT_HEAP_SIZE)).unwrap_or(DEFAULT_HEAP_SIZE);
+    let stack_size = env::var("TEENSY4_STACK_SIZE").map(|val| val.parse::<usize>().unwrap_or(DEFAULT_STACK_SIZE)).unwrap_or(DEFAULT_STACK_SIZE);
 
     RuntimeBuilder::from_flexspi(Family::Imxrt1060, 1984 * 1024)
         .flexram_banks(FlexRamBanks {
@@ -9,9 +18,9 @@ fn main() {
             dtcm: 10,
         })
         .heap(Memory::Ocram)
-        .heap_size(16 * 1024)
+        .heap_size(heap_size)
         .stack(Memory::Dtcm)
-        .stack_size(16 * 1024)
+        .stack_size(stack_size)
         .vectors(Memory::Dtcm)
         .text(Memory::Itcm)
         .data(Memory::Dtcm)
