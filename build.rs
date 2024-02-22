777f72426a39607f5d8e951d7482898b35ec111c
Ben Stern <bstern@fortian.com>
2024-02-22 00:43:05 -0500

    panic on parse failure but recognize K/k suffix

--- build.rs
+++ build.rs
@@ -2,20 +2,22 @@
 use std::env;
 
 #[cfg(feature = "rt")]
-const DEFAULT_HEAP_SIZE: usize = 16 * 1024;
-#[cfg(feature = "rt")]
-const DEFAULT_STACK_SIZE: usize = 16 * 1024;
+fn parse_size_string(val: Result<String, env::VarError>, default: usize) -> usize {
+    val.map(|val| {
+        if val.ends_with('k') || val.ends_with('K') {
+            val[.. val.len() - 1].parse::<usize>().unwrap() * 1024
+        } else {
+            val.parse::<usize>().unwrap()
+        }
+    }).unwrap_or(default)
+}
 
 #[cfg(feature = "rt")]
 fn main() {
     use imxrt_rt::{Family, FlexRamBanks, Memory, RuntimeBuilder};
 
-    let heap_size = env::var("TEENSY4_HEAP_SIZE")
-        .map(|val| val.parse::<usize>().unwrap_or(DEFAULT_HEAP_SIZE))
-        .unwrap_or(DEFAULT_HEAP_SIZE);
-    let stack_size = env::var("TEENSY4_STACK_SIZE")
-        .map(|val| val.parse::<usize>().unwrap_or(DEFAULT_STACK_SIZE))
-        .unwrap_or(DEFAULT_STACK_SIZE);
+    let heap_size = parse_size_string(env::var("TEENSY4_HEAP_SIZE"), 16 * 1024);
+    let stack_size = parse_size_string(env::var("TEENSY4_STACK_SIZE"), 16 * 1024);
 
     RuntimeBuilder::from_flexspi(Family::Imxrt1060, 1984 * 1024)
         .flexram_banks(FlexRamBanks {
