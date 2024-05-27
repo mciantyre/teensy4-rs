650dcff39b848a07ed8d1232a06dcef0c959e4a4
mciantyre <mciantyre@users.noreply.github.com>
2024-05-27 23:12:38 +0000

    deploy: e07527192e8475e0552926c2e41b52ffb2ca51b4

--- teensy4_pins/sidebar-items.js
+++ teensy4_pins/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["common","t40","t41","tmm"]};
+window.SIDEBAR_ITEMS = {"enum":["DriveStrength","Hysteresis","OpenDrain","PullKeeper","SlewRate","Speed"],"fn":["configure"],"mod":["common","t40","t41","tmm"],"struct":["Config"]};
