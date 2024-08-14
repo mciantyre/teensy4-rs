384b3281a73a8beecb09e18828c3933e5a9838a1
sigil-03 <sigil@glyphs.tech>
2024-08-14 16:06:17 -0600

    add tx and rx handler

--- examples/rtic_can_log.rs
+++ examples/rtic_can_log.rs
@@ -30,7 +30,7 @@ mod app {
 
     use imxrt_log as logging;
 
-    use rtic_monotonics::systick::*;
+    use rtic_monotonics::systick::{self, *};
 
     // FIXME: need these?
 
@@ -53,11 +53,12 @@ mod app {
         led: board::Led,
         // q_rx: Consumer,
         // blink_count: u32,
-        can: board::Flexcan1,
     }
 
     #[shared]
-    struct Shared {}
+    struct Shared {
+        can: board::Flexcan1,
+    }
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
@@ -80,9 +81,9 @@ mod app {
         let mut can = board::flexcan(flexcan1, pins.p22, pins.p23);
         can.set_baud_rate(125_000);
         can.set_max_mailbox(16);
-        can.enable_fifo();
-        can.set_fifo_interrupt(true);
-        can.set_fifo_accept_all();
+        can.disable_fifo();
+        // can.set_fifo_interrupt(true);
+        // can.set_fifo_accept_all();
         // Set up a system timer for our software task.
         {
             Systick::start(
@@ -93,7 +94,7 @@ mod app {
         }
 
         // Schedule that software task to run.
-        make_logs::spawn().unwrap();
+        // make_logs::spawn().unwrap();
 
         // The queue used for buffering bytes.
         // let (_q_tx, q_rx) = cx.local.queue.split();
@@ -111,32 +112,41 @@ mod app {
         //        can1_init::spawn_after(1_u32.secs()).unwrap();
 
         // Schedule the first blink.
-        // blink::spawn_after(1_u32.secs()).unwrap();
+        blink::spawn().unwrap();
 
-        run_can::spawn().unwrap();
+        run_can_rx::spawn().unwrap();
+        run_can_tx::spawn().unwrap();
         // If the LED turns on, we've made it past init.
         led.set();
 
-        (Shared {}, Local { poller, led, can })
+        (Shared { can }, Local { poller, led })
     }
 
-    #[task(local = [can])]
-    async fn run_can(cx: run_can::Context) {
-        let run_can::LocalResources { can, .. } = cx.local;
+    #[task(shared = [can])]
+    async fn run_can_rx(cx: run_can_rx::Context) {
+        let run_can_rx::SharedResources { mut can, .. } = cx.shared;
+        loop {
+            // read all available mailboxes for any available frames
+            if let Some(data) = can.lock(|can| can.read_mailboxes()) {
+                log::info!("RX: MB{} - {:?}", data.mailbox_number, data.frame);
+            }
+
+            Systick::delay(1.millis()).await;
+        }
+    }
+
+    #[task(shared = [can])]
+    async fn run_can_tx(cx: run_can_tx::Context) {
+        let run_can_tx::SharedResources { mut can, .. } = cx.shared;
         // create a `Frame` with `StandardID` 0x00
         // and `Data` [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]
-        let id = imxrt_hal::can::Id::from(imxrt_hal::can::StandardId::new(0x00).unwrap());
-        let data: [u8; 8] = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07];
+        let id = imxrt_hal::can::Id::from(imxrt_hal::can::StandardId::new(0x03).unwrap());
+        let data: [u8; 1] = [0x03];
         let frame = imxrt_hal::can::Frame::new_data(id, data);
 
         loop {
             // read all available mailboxes for any available frames
-            can.read_mailboxes();
-
-            // transmit the frame
-            can.transmit(&frame);
-            log::info!("Transmit");
-
+            can.lock(|can| can.transmit(&frame));
             Systick::delay(1000.millis()).await;
         }
     }
@@ -145,36 +155,36 @@ mod app {
     ///
     /// You won't see all the log levels until you configure your build. See the
     /// top-level docs for more information.
-    #[task(local = [led])]
-    async fn make_logs(cx: make_logs::Context) {
-        let make_logs::LocalResources { led, .. } = cx.local;
+    // #[task(local = [led])]
+    // async fn make_logs(cx: make_logs::Context) {
+    //     let make_logs::LocalResources { led, .. } = cx.local;
 
-        let mut counter = 0u32;
-        loop {
-            led.toggle();
-            Systick::delay(250.millis()).await;
+    //     let mut counter = 0u32;
+    //     loop {
+    //         led.toggle();
+    //         Systick::delay(250.millis()).await;
 
-            log::trace!("TRACE: {}", counter);
+    //         log::trace!("TRACE: {}", counter);
 
-            if counter % 3 == 0 {
-                log::debug!("DEBUG: {}", counter);
-            }
+    //         if counter % 3 == 0 {
+    //             log::debug!("DEBUG: {}", counter);
+    //         }
 
-            if counter % 5 == 0 {
-                log::info!("INFO: {}", counter);
-            }
+    //         if counter % 5 == 0 {
+    //             log::info!("INFO: {}", counter);
+    //         }
 
-            if counter % 7 == 0 {
-                log::warn!("WARN: {}", counter);
-            }
+    //         if counter % 7 == 0 {
+    //             log::warn!("WARN: {}", counter);
+    //         }
 
-            if counter % 31 == 0 {
-                log::error!("ERROR: {}", counter);
-            }
+    //         if counter % 31 == 0 {
+    //             log::error!("ERROR: {}", counter);
+    //         }
 
-            counter = counter.wrapping_add(1);
-        }
-    }
+    //         counter = counter.wrapping_add(1);
+    //     }
+    // }
 
     /// This task runs when the USB1 interrupt activates.
     /// Simply poll the logger to control the logging process.
@@ -183,26 +193,14 @@ mod app {
         cx.local.poller.poll();
     }
 
-    //     #[task(local = [led, q_rx, blink_count])]
-    //     async fn blink(cx: blink::Context) {
-    //         if cx.local.q_rx.ready() {
-    //             let mut buffer = [0u8; 256];
-    //             for elem in buffer.iter_mut() {
-    //                 *elem = match cx.local.q_rx.dequeue() {
-    //                     None => break,
-    //                     Some(b) => b,
-    //                 };
-    //             }
-    //             let s = core::str::from_utf8(&buffer).unwrap();
-    //             log::info!("read: {}", s);
-    //         }
-    //
-    //         // Toggle the LED.
-    //         cx.local.led.toggle();
-    //
-    //         // Schedule the following blink.
-    //         blink::spawn_after(100_u32.millis()).unwrap();
-    //     }
+    #[task(local = [led])]
+    async fn blink(cx: blink::Context) {
+        loop {
+            // Toggle the LED.
+            cx.local.led.toggle();
+            Systick::delay(1000.millis()).await;
+        }
+    }
 
     //    #[task(shared = [can1])]
     //    async fn can1_init(mut cx: can1_init::Context) {
